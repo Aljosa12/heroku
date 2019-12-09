926c0a023ca70651a6d307bee21e1dec2ea42263
Aljosa Lazarevic Gostincar <lgaljo@gmail.com>
2019-12-09 17:23:24 +0100

    update

---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+import datetime
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    trenutni_cas = datetime.datetime.now()
+    return f"<h1>Moja prva spletna stran</h1><h2>manjši naslov</h2><p>lorem ipsum</p>{trenutni_cas}"
+
+@app.route("/about")
+def on_about():
+    # Preberemo datoteko:
+    return render_template("index.html")
+
+@app.route("/home")
+def home():
+    # Preberemo datoteko:
+    return render_template("Bootstrap.html")
+
+
+
+if __name__== "__main__":
+    app.run()
